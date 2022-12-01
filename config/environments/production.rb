e4eccc8911a9e504ce91f42a8be31aa37d4cfd75
ALASTAIR <58214831+alidriver-github@users.noreply.github.com>
2022-12-01 19:04:54 +0000

    done for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,9 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.hosts = [
+    "alidriver-github-upgraded-bassoon-59gjqwxqqphpp6w-3000.preview.app.github.dev"
+  ]
   # Code is not reloaded between requests.
   config.cache_classes = true
 
